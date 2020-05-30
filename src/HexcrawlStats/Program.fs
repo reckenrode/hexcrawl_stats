0d0fdb897e1b764b1be8da83f296a26797c2ae91
Randy Eckenrode <randy@largeandhighquality.com>
2020-05-30 00:09:02 -0400

    Implement generation and statistics printing
    - Generate a ton of data
    - Use Deedle to aggregate and pivot it
    
    The goal is to determine across what percentage of rolls and how many times per adventuring day a type of encounter is generated.

--- src/HexcrawlStats/Program.fs
+++ src/HexcrawlStats/Program.fs
@@ -14,9 +14,72 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+module HexcrawlStats.Main
+
+open Deedle
+open FsRandom
+open Hexcrawl
 open System
+
+let rollsForDay = List.randomCreate 6 Hexcrawl.maybeEncounter
+
+let isExploring x = 2 <= x && x <= 4
+
+let cpus = System.Environment.ProcessorCount
+let iterations = 1_000_000
+
+let generateValues t =
+    let inline filterAndPrepareDF (row, results) =
+        let row = (t <<< 28) + row
+        results
+        |> Seq.mapi (fun idx  x ->
+            match x with
+            | Some (WanderingMonster _) -> ((row, idx), "Type", "wandering monster")
+            | Some (Discovery (Lair _)) when isExploring idx -> ((row, idx), "Type", "lair")
+            | Some (Discovery (Tracks _)) when isExploring idx -> ((row, idx), "Type", "tracks")
+            | Some (Discovery Location) when isExploring idx -> ((row, idx), "Type", "location")
+            | _ -> ((row, idx), "Type", "no encounter"))
+    async {
+        return Utility.createRandomState ()
+            |> Seq.ofRandom rollsForDay
+            |> Seq.take iterations
+            |> Seq.indexed
+            |> Seq.collect filterAndPrepareDF
+            |> Frame.ofValues
+    }
+
+let getBreakdown column =
+    column
+    |> Series.groupBy (fun _ t -> t)
+    |> Series.map (fun _ t ->
+        Math.Round ((t |> Stats.count |> float) * 100.0 / (cpus * iterations |> float), 1))
+    :> ISeries<obj>
+
+let getRawData () =
+    Array.init cpus generateValues
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Frame.mergeAll
+
+let calculateFrequencyPerDay data =
+    let cols = data |> Frame.cols
+    let breakdowns = cols |> Series.values |> Seq.map getBreakdown
+    Frame (cols |> Series.keys, breakdowns)
+    |> Frame.dropCol "no encounter"
+    |> Frame.transpose
+    |> Frame.fillMissingWith 0.0
+    |> Frame.sortColsByKey
+    |> Frame.sortRowsByKey
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let data =
+        getRawData ()
+        |> Frame.pivotTable
+            (fun (day, _) _ -> day)
+            (fun _ r -> r.GetAs<string> "Type")
+            Frame.countRows
+        |> Frame.fillMissingWith 0.0
+        |> calculateFrequencyPerDay
+    data.Print ()
     0 // return an integer exit code
